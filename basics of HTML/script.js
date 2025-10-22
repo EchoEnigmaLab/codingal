5d2ceb6a1011c9325a6b782ca732c8814303c619
EchoEnigmaLab <bushraansari@codingal>
2025-10-22 22:05:31 +0530

    main

--- basics of HTML/script.js
+++ basics of HTML/script.js
@@ -1,27 +1,4 @@
-function validate(e){
-    e.preventDefault();
-
-    const email = document.getElementById('email').value;
-    const pass = document.getElementById('password').value;
-    const age = document.getElementById('age').value;
-    const msgBox= document.getElementById('message');
-
-    let message = '';
-
-    if(email===''){
-        message = ' enter your email';
-        msgBox.style.color='red';
-    } else if (pass===''){
-        message = ' enter your password';
-        msgBox.style.color = 'red';
-    } else if (age === ''){
-        message='Please enter your age';
-        msgBox.style.color = 'red';
-    } else{
-        message = "login suucessfull"
-        msgBox.style.color = 'green';
-    }
-
-    msgBox.innerText= message;
-
+function myfunction(){
+    var a = 5;
+    document.getElementById("result").innerHTML= a*a;
 }
